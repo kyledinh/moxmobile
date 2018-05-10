8d309cbe07a6c19d218cbe69c2408c55c84e402f
Kyle Dinh <kyledinh@gmail.com>
2018-05-10 16:42:01 -0700

    moved to es6 syntax for functions

--- app/redux/actions.js
+++ app/redux/actions.js
@@ -1,12 +1,7 @@
 import { Sys } from '../System';
 import initialdata from  '../data/initialdata';
 
-export function fetchPlayersFromAPI() {
-    // return (dispatch) => {
-    //     dispatch(getPlayers());
-    //     dispatch(getPlayersSuccess(initialdata.players));
-    // }
-
+export const fetchPlayersFromAPI = () => {
   return (dispatch) => {
     dispatch(getPlayers());
     fetch('https://maalbox.com/washer?name=MOX_RFN&wins=MOX_RI_100&points=MOX_RI_1000',
@@ -20,91 +15,70 @@ export function fetchPlayersFromAPI() {
       })
       .catch(err => dispatch(getPlayersFailure(err)));
   }
-
 }
 
-export function getPlayers() {
-  return {
-    type: Sys.FETCHING_PLAYERS
-  }
-}
+export const getPlayers = () => ({
+  type: Sys.FETCHING_PLAYERS
+});
 
-export function addPlayer(data) {
-  return {
-    type: Sys.PLAYER_ADD_NEW,
-    data
-  }
-}
+export const addPlayer = (data) => ({
+  type: Sys.PLAYER_ADD_NEW,
+  data
+});
 
-export function getPlayersSuccess(data) {
-  return {
-    type: Sys.FETCHING_PLAYERS_SUCCESS,
-    data,
-  }
-}
+export const getPlayersSuccess = (data) => ({
+  type: Sys.FETCHING_PLAYERS_SUCCESS,
+  data,
+});
 
-export function getPlayersFailure() {
-  return {
-    type: Sys.FETCHING_PLAYERS_FAILURE
-  }
-}
+export const getPlayersFailure = () => ({
+  type: Sys.FETCHING_PLAYERS_FAILURE
+});
 
 // TEAMS
-export function fetchTeamsFromAPI() {
+export const fetchTeamsFromAPI = () => {
   return (dispatch) => {
     dispatch(getTeams());
     dispatch(getTeamsSuccess(initaldata.teams));
   }
 }
 
-export function getTeams() {
-  return {
-    type: Sys.FETCHING_TEAMS
-  }
-}
+export const getTeams = () => ({
+  type: Sys.FETCHING_TEAMS
+});
 
-export function getTeamsSuccess(data) {
-  return {
-    type: Sys.FETCHING_TEAMS_SUCCESS,
-    data,
-  }
-}
+export const getTeamsSuccess = (data) => ({
+  type: Sys.FETCHING_TEAMS_SUCCESS,
+  data,
+});
 
-export function getTeamsFailure() {
-  return {
-    type: Sys.FETCHING_TEAMS_FAILURE
-  }
-}
+export const getTeamsFailure = () => ({
+  type: Sys.FETCHING_TEAMS_FAILURE
+});
 
 // MATCHES
-export function fetchMatchesFromAPI() {
+export const fetchMatchesFromAPI = () => {
   return (dispatch) => {
     dispatch(getMatches());
     dispatch(getMatchesSuccess(initialdata.matches));
   }
-}
+};
 
-export function getMatches() {
-  return {
-    type: Sys.FETCHING_MATCHES
-  }
-}
+export const getMatches = () => ({
+  type: Sys.FETCHING_MATCHES
+});
 
-export function getMatchesSuccess(data) {
-  return {
-    type: Sys.FETCHING_MATCHES_SUCCESS,
-    data,
-  }
-}
+export const getMatchesSuccess = (data) => ({
+  type: Sys.FETCHING_MATCHES_SUCCESS,
+  data,
+});
 
-export function getMatchesFailure() {
-  return {
-    type: Sys.FETCHING_MATCHES_FAILURE
-  }
-}
+export const getMatchesFailure = () => ({
+  type: Sys.FETCHING_MATCHES_FAILURE
+});
 
 // NEWS
-export function fetchNewsFromAPI() {
+export const fetchNewsFromAPI = () => {
   return (dispatch) => {
     dispatch(getNews());
     fetch('https://api.openaq.org/v1/cities?limit=10&country=NL')
@@ -119,21 +93,15 @@ export function fetchNewsFromAPI() {
 
 // {"meta":{"name":"openaq-api","license":"CC BY 4.0","website":"https://docs.openaq.org/","page":1,"limit":10,"found":67},"results":[{"city":"Zaandam","country":"NL","locations":1,"count":71665},{"city":"Amsterdam","country":"NL","locations":14,"count":643338},{"city":"Hoek v. Holland","country":"NL","locations":1,"count":71660},{"city":"Wieringerwerf","country":"NL","locations":1,"count":71671},{"city":"Rotterdam","country":"NL","locations":3,"count":143334},{"city":"Leiden","country":"NL","locations":1,"count":1620},{"city":"Schiedam","country":"NL","locations":2,"count":71663},{"city":"Rotterdam Noord ","country":"NL","locations":1,"count":53745},{"city":"De Rijp","country":"NL","locations":2,"count":17924},{"city":"Rotterdam Zuid","country":"NL","locations":2,"count":89591}]}
 
-export function getNews() {
-  return {
-    type: Sys.FETCHING_NEWS
-  }
-}
+export const getNews = () => ({
+  type: Sys.FETCHING_NEWS
+});
 
-export function getNewsSuccess(data) {
-  return {
-    type: Sys.FETCHING_NEWS_SUCCESS,
-    data,
-  }
-}
+export const getNewsSuccess = (data) => ({
+  type: Sys.FETCHING_NEWS_SUCCESS,
+  data,
+});
 
-export function getNewsFailure() {
-  return {
-    type: Sys.FETCHING_NEWS_FAILURE
-  }
-}
+export const getNewsFailure = () => ({
+  type: Sys.FETCHING_NEWS_FAILURE
+});
